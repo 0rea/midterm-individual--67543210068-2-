7a96e3fd4dc25b813a2d3d652802308de78add69
67543210068-2 <Nattapong@admins-iMac.local>
2026-01-14 10:45:20 +0700

    docs: add README and architecture documentation

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express = require('express');
 const app = express();
 
 const studentRoutes = require('./src/presentation/routes/student.routes');
+const errorMiddleware = require('./src/presentation/middlewares/error.middleware');
 
 app.use(express.json());
-
 app.use('/api/students', studentRoutes);
+
+// error handler (middleware)
+app.use(errorMiddleware);
 
 const PORT = 3000;
 app.listen(PORT, () => {
